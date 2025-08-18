8edddc79a3f5bd955f829572b0d6b7054d82bb46
= <forsvantn@protonmail.com>
2025-08-18 13:43:23 +0200

    [Spring 1][Task 4] feat: Split auth service into lib and bin crate to make it ready for testing

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,30 @@
+use axum::{response::Html, routing::get, serve::Serve, Router};
+use std::error::Error;
+use tower_http::services::ServeDir;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/hello", get(hello_handler));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("This is a custom hello message!")
+}
