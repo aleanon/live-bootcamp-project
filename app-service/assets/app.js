f35e2a0e1944b19257480eb09da2d619ae9dd569
= <forsvantn@protonmail.com>
2025-08-23 20:59:06 +0200

    [Sprint 1][Upgrade to https] fix: Changed assets paths to work with reverse-proxy, and fixed Caddyfile

--- app-service/assets/app.js
+++ app-service/assets/app.js
@@ -14,7 +14,7 @@ logoutLink.addEventListener("click", (e) => {
     if (response.ok) {
       loginLink.style.display = "block";
       logoutLink.style.display = "none";
-      protectImg.src = "/app/assets/default.jpg";
+      protectImg.src = "/assets/default.jpg";
     } else {
       alert("Failed to logout");
     }
@@ -22,7 +22,7 @@ logoutLink.addEventListener("click", (e) => {
 });
 
 (() => {
-  fetch("/app/protected").then((response) => {
+  fetch("/protected").then((response) => {
     if (response.ok) {
       loginLink.style.display = "none";
       logoutLink.style.display = "block";
@@ -32,13 +32,13 @@ logoutLink.addEventListener("click", (e) => {
         if (img_url !== undefined && img_url !== null && img_url !== "") {
           protectImg.src = img_url;
         } else {
-          protectImg.src = "/app/assets/default.jpg";
+          protectImg.src = "/assets/default.jpg";
         }
       });
     } else {
       loginLink.style.display = "block";
       logoutLink.style.display = "none";
-      protectImg.src = "/app/assets/default.jpg";
+      protectImg.src = "/assets/default.jpg";
     }
   });
 })();
