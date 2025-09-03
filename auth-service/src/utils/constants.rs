f37f22f2da74c7f9a6d567b4898f95b8b6315d38
= <forsvantn@protonmail.com>
2025-09-03 12:30:06 +0200

    [Sprint 3][Task 6] fix: using LazyLock instead of lazy static macro to fix deployment issue

--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -1,11 +1,8 @@
 use dotenvy::dotenv;
-use lazy_static::lazy_static;
-use std::env as std_env;
+use std::{env as std_env, sync::LazyLock};
 
 // Define a lazily evaluated static. lazy_static is needed because std_env::var is not a const function.
-lazy_static! {
-    pub static ref JWT_SECRET: String = set_token();
-}
+pub static JWT_SECRET: LazyLock<String> = LazyLock::new(|| set_token());
 
 fn set_token() -> String {
     dotenv().ok(); // Load environment variables
