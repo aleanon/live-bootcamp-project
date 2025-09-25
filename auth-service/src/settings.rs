1537826ee36d753b5ce1fb86740a26d372605f08
= <forsvantn@protonmail.com>
2025-09-25 12:04:12 +0200

    Fix: Changed gh actions to use old compose file for teardown and new for rebuild in the droplet

--- auth-service/src/settings.rs
+++ auth-service/src/settings.rs
@@ -1,4 +1,4 @@
-use std::{ops::Deref, sync::Arc};
+use std::{ops::Deref, sync::Arc, time::Duration};
 
 use axum::http::HeaderValue;
 use dashmap::DashSet;
@@ -10,16 +10,44 @@ use tokio::sync::{RwLock, RwLockReadGuard};
 use crate::utils::constants::AUTH_SERVICE_ALLOWED_ORIGINS;
 
 #[derive(Debug, Deserialize)]
+pub struct JWTSettings {
+    pub cookie_name: String,
+    pub secret: Secret<String>,
+    pub time_to_live: Duration,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AuthSettings {
+    pub jwt: JWTSettings,
+    pub elevated_jwt: JWTSettings,
+    pub allowed_origins: AllowedOrigins,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct EmailClient {
+    pub base_url: String,
+    pub sender: String,
+    pub timeout: Duration,
+    pub auth_token: Secret<String>,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Postgres {
-    username: Secret<String>,
-    password: Secret<String>,
+    pub username: Secret<String>,
+    pub password: Secret<String>,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct Redis {
-    host_name: String,
+    pub host_name: String,
 }
 
-pub(crate) struct Settings {}
+pub(crate) struct Settings {
+    pub auth: AuthSettings,
+    pub email_client: EmailClient,
+    pub postgres: Postgres,
+    pub redis: Redis,
+}
 
 // #[derive(Clone)]
 // pub struct Settings(Arc<RwLock<Config>>);
@@ -62,60 +90,60 @@ pub(crate) struct Settings {}
 // #[allow(unused)]
 // struct RedisConfig {}
 
-// #[derive(Debug, Clone)]
-// pub struct AllowedOrigins(Arc<DashSet<HeaderValue>>);
+#[derive(Debug, Clone)]
+pub struct AllowedOrigins(Arc<DashSet<HeaderValue>>);
 
-// impl AllowedOrigins {
-//     pub fn new(headers: Arc<DashSet<HeaderValue>>) -> Self {
-//         AllowedOrigins(headers)
-//     }
-// }
+impl AllowedOrigins {
+    pub fn new(headers: Arc<DashSet<HeaderValue>>) -> Self {
+        AllowedOrigins(headers)
+    }
+}
 
-// impl Default for AllowedOrigins {
-//     fn default() -> Self {
-//         dotenv().ok();
-//         let allowed_origins = AUTH_SERVICE_ALLOWED_ORIGINS
-//             .split(',')
-//             .filter_map(|origin| origin.trim().parse().ok())
-//             .collect::<DashSet<_>>();
+impl Default for AllowedOrigins {
+    fn default() -> Self {
+        dotenv().ok();
+        let allowed_origins = AUTH_SERVICE_ALLOWED_ORIGINS
+            .split(',')
+            .filter_map(|origin| origin.trim().parse().ok())
+            .collect::<DashSet<_>>();
 
-//         AllowedOrigins(Arc::new(allowed_origins))
-//     }
-// }
+        AllowedOrigins(Arc::new(allowed_origins))
+    }
+}
 
-// impl Serialize for AllowedOrigins {
-//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-//     where
-//         S: serde::Serializer,
-//     {
-//         let headers = self
-//             .0
-//             .iter()
-//             .filter_map(|header_value| header_value.to_str().and_then(|h| Ok(h.to_owned())).ok())
-//             .collect::<DashSet<_>>();
+impl Serialize for AllowedOrigins {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let headers = self
+            .0
+            .iter()
+            .filter_map(|header_value| header_value.to_str().and_then(|h| Ok(h.to_owned())).ok())
+            .collect::<DashSet<_>>();
 
-//         headers.serialize(serializer)
-//     }
-// }
+        headers.serialize(serializer)
+    }
+}
 
-// impl<'de> Deserialize<'de> for AllowedOrigins {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-//     where
-//         D: serde::Deserializer<'de>,
-//     {
-//         let headers = Vec::<String>::deserialize(deserializer)?
-//             .iter()
-//             .filter_map(|value| value.parse().ok())
-//             .collect();
+impl<'de> Deserialize<'de> for AllowedOrigins {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let headers = Vec::<String>::deserialize(deserializer)?
+            .iter()
+            .filter_map(|value| value.parse().ok())
+            .collect();
 
-//         Ok(AllowedOrigins::new(Arc::new(headers)))
-//     }
-// }
+        Ok(AllowedOrigins::new(Arc::new(headers)))
+    }
+}
 
-// impl Deref for AllowedOrigins {
-//     type Target = Arc<DashSet<HeaderValue>>;
+impl Deref for AllowedOrigins {
+    type Target = Arc<DashSet<HeaderValue>>;
 
-//     fn deref(&self) -> &Self::Target {
-//         &self.0
-//     }
-// }
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
