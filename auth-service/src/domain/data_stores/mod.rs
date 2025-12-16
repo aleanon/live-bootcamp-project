1d8a7111166c002143ae2f50df65d40dfa2a3f66
= <forsvantn@protonmail.com>
2025-12-16 09:30:29 +0100

    Removed unused trait

--- auth-service/src/domain/data_stores/mod.rs
+++ auth-service/src/domain/data_stores/mod.rs
@@ -1,9 +1,7 @@
-mod allowed_origins_store;
 mod banned_token_store;
 mod two_fa_code_store;
 mod user_store;
 
-pub use allowed_origins_store::{AllowedOriginsStore, AllowedOriginsStoreError};
 pub use banned_token_store::{BannedTokenStore, BannedTokenStoreError};
 pub use two_fa_code_store::{TwoFaCodeStore, TwoFaCodeStoreError};
 pub use user_store::{UserStore, UserStoreError};
